456995740648855f6da646d09b6337c7de9eb73b
Prakash <prakashru@gmail.com>
2020-08-29 01:02:38 +0530

    second commit with few changes....

--- src/main/java/org/pictolearn/docker/HelloWorldPing.java
+++ src/main/java/org/pictolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 100; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Hello World Ping --> version 1 " + i );
 			Thread.sleep(1000);
 		}
 	}
